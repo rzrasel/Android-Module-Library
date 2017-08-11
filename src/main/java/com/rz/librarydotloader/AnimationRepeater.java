46b516fcfbcb3c60399e27b476fca94fce3092ce
Rz Rasel <Rz Rasel>
2017-08-11 21:11:45 +0600

    Add Project Library Dot Loader

---
+++ src/main/java/com/rz/librarydotloader/AnimationRepeater.java
@@ -0,0 +1,47 @@
+package com.rz.librarydotloader;
+
+/**
+ * Created by Rz Rasel on 2017-07-07.
+ */
+
+import android.animation.Animator;
+
+class AnimationRepeater implements Animator.AnimatorListener {
+    private boolean alternate = true;
+    private Dot mDot;
+    private Integer[] mColors;
+
+    AnimationRepeater(Dot dot, Integer[] colors) {
+        this.mDot = dot;
+        mColors = colors;
+    }
+
+    @Override
+    public void onAnimationStart(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animator) {
+        if (alternate) {
+            mDot.colorAnimator.setObjectValues(
+                    mColors[mDot.mCurrentColorIndex],
+                    mColors[mDot.incrementColorIndex()]
+            );
+            mDot.colorAnimator.start();
+            alternate = false;
+        } else {
+            alternate = true;
+        }
+    }
+}
